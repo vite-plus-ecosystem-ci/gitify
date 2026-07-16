4be30397d9dc3f7478d06968c0ec4be9cfd57509
wan9chi <dk4rest@gmail.com>
2026-07-16 14:44:15 +0800

    test: vite-plus prerelease 0.0.0-commit.bb0445bf5599a389c324442a3f3f8a8a5db5d83d

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite-plus';
 
 export default defineConfig({
   plugins: [react()],
